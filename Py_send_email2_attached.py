bf51f818b7538bcf640abbd0a423d5e8ee514380
mikexie <mxie622@gmail.com>
2019-02-13 00:56:01 +1300

    Send email with attached files

--- Py_send_email2_attached.py
+++ Py_send_email2_attached.py
@@ -1,4 +1,4 @@
-import urllib, urllib2
+# import urllib, urllib2
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -9,19 +9,19 @@ password = 'xms19911227'
 sender = username
 receivers = ','.join(['mxie622@aucklanduni.ac.nz'])
 
-# 如名字所示： Multipart就是多个部分
+# Multipart
 msg = MIMEMultipart()
 msg['Subject'] = 'Python mail Test'
 msg['From'] = sender
 msg['To'] = receivers
 
-# 下面是文字部分，也就是纯文本
+# pure text
 puretext = MIMEText('text_part，')
 msg.attach(puretext)
 
-# 下面是附件部分 ，这里分为了好几个类型
+# attached
 
-# 首先是xlsx类型的附件
+# xlsx
 xlsxpart = MIMEApplication(open('diabetes.xlsx', 'rb').read())
 xlsxpart.add_header('Content-Disposition', 'attachment', filename='diabetes.xlsx')
 msg.attach(xlsxpart)
@@ -31,29 +31,29 @@ txtpart = MIMEApplication(open('stopwords.txt', 'rb').read())
 txtpart.add_header('Content-Disposition', 'attachment', filename='stopwords.txt')
 msg.attach(txtpart)
 
-# jpg类型的附件
+# jpg
 # jpgpart = MIMEApplication(open('beauty.jpg', 'rb').read())
 # jpgpart.add_header('Content-Disposition', 'attachment', filename='beauty.jpg')
 # msg.attach(jpgpart)
 
-# mp3类型的附件
+# mp3
 # mp3part = MIMEApplication(open('kenny.mp3', 'rb').read())
 # mp3part.add_header('Content-Disposition', 'attachment', filename='benny.mp3')
 # msg.attach(mp3part)
 
-##  下面开始真正的发送邮件了
+##  sending
 try:
     client = smtplib.SMTP()
     client.connect('smtp.gmail.com')
     client.login(username, password)
     client.sendmail(sender, receivers, msg.as_string())
     client.quit()
-    print('带有各种附件的邮件发送成功！')
+    print('success！')
 except smtplib.SMTPRecipientsRefused:
     print('Recipient refused')
 except smtplib.SMTPAuthenticationError:
     print('Auth error')
 except smtplib.SMTPSenderRefused:
     print('Sender refused')
-except smtplib.SMTPException,e:
-    print (e.message)
+# except smtplib.SMTPException,e:
+#     print(e.message)
